d84b76c0acd0e9090865f59736f548451b5967a3
Lewis Contreras <legiconba4@gmail.com>
2025-01-01 23:27:45 -0500

    refac(login)#2: mod login form
    - Use formik to handle form
    - Use FormikInput component

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,29 +1,36 @@
 import { Button } from "@chakra-ui/button";
 import { Img } from "@chakra-ui/image";
-import { Input } from "@chakra-ui/input";
-import { Link } from "@chakra-ui/layout";
-import { HStack } from "@chakra-ui/layout";
-import { VStack } from "@chakra-ui/layout";
-import { Text } from "@chakra-ui/layout";
-import { Center, Box } from "@chakra-ui/layout";
+import { VStack, Text, Center, Box, Link, HStack } from "@chakra-ui/layout";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link as ReachLink } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import { startGoogleLogin, startLoginEmailPassword } from "../../actions/auth";
-import "../../styles/animations.css";
 import { FaGoogle } from "react-icons/fa";
 import { Icon } from "@chakra-ui/react";
+import { FormikInput } from "../FormikInput";
 
 function LoginScreen() {
   const dispatch = useDispatch();
   const { loading } = useSelector((state) => state.ui);
 
-  const handleSubmitLogin = (e) => {
-    e.preventDefault();
-    let email = document.getElementById("email-login").value;
-    let password = document.getElementById("password-login").value;
-    dispatch(startLoginEmailPassword(email, password));
-  };
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Debe ser un correo válido")
+        .required("El correo es obligatorio"),
+      password: Yup.string().required("La contraseña es obligatoria"),
+    }),
+    onSubmit: (values) => {
+      const { email, password } = values;
+      dispatch(startLoginEmailPassword(email, password));
+    },
+  });
 
   const handleGoogleLogin = () => {
     dispatch(startGoogleLogin());
@@ -33,53 +40,51 @@ function LoginScreen() {
     <Center h="100vh">
       <Box borderRadius="10px" p="1px" w="350px" bgColor="brand.primary">
         <Center h="120px" borderRadius="10px 10px 0 0">
-          <Img src="https://i.imgur.com/pwIYVhf.png" />
+          <Img src="https://i.imgur.com/pwIYVhf.png" alt="Logo de Blockmaster" />
         </Center>
-        <form onSubmit={handleSubmitLogin}>
           <VStack
+          as="form"
+            onSubmit={formik.handleSubmit}
             className="scene_element scene_element--clipmidup"
             p="10px"
             h="340px"
             borderRadius="0 0 10px 10px"
-            spacing="10px"
+            spacing="15px"
             bgColor="#14112e"
           >
             <Text my="10px" fontWeight="600" fontSize="xl">
               Ingresa con correo y contraseña
             </Text>
-            <Input
-              id="email-login"
+            <FormikInput
+              name="email"
               type="email"
               placeholder="Email"
-              borderColor="brand.primary"
-              color="white"
-              isRequired
+              formik={formik}
             />
-            <Input
-              id="password-login"
+            <FormikInput
+              name="password"
               type="password"
-              borderColor="brand.primary"
               placeholder="Password"
-              isRequired
+              formik={formik}
             />
-            <Button disabled={loading} type="submit" bgColor="#3C5EEA">
+            <Button
+              disabled={loading}
+              type="submit"
+              bgColor="#3C5EEA"
+              isLoading={loading}
+            >
               Entrar
             </Button>
-
-            <Box textAlign="center" width="100%">
               <HStack
                 onClick={handleGoogleLogin}
                 cursor="pointer"
-                position="relative"
-                spacing="20px"
                 px="6px"
-                my="20px"
                 borderRadius="10px"
                 bgColor="white"
                 h="40px"
                 width="100%"
               >
-                <Icon as={FaGoogle} fontSize="20px" color={"#3C5EEA"} />
+                <Icon as={FaGoogle} fontSize="20px" color="#3C5EEA" />
                 <Text
                   fontWeight="600"
                   textAlign="center"
@@ -92,9 +97,7 @@ function LoginScreen() {
               <Link to="/auth/register" as={ReachLink}>
                 ¿No estás registrado?
               </Link>
-            </Box>
           </VStack>
-        </form>
       </Box>
     </Center>
   );
