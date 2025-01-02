dcd469d1ecc175c8e63be61561b3e0d9bb0b0784
Lewis Contreras <legiconba4@gmail.com>
2025-01-02 13:42:40 -0500

    refac(crud)#2: mod crud search
    - Use useRef instead of form to manage input value

--- src/components/crud/SearchCrud.jsx
+++ src/components/crud/SearchCrud.jsx
@@ -1,5 +1,5 @@
 import { Box, Button, HStack, Input, Text } from "@chakra-ui/react";
-import React from "react";
+import React, { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { crudSearch } from "../../actions/crudActions";
 import { db } from "../../firebase/firebase-config";
@@ -8,11 +8,10 @@ import { GoHomeIcon } from "./GoHomeIcon";
 const SearchCrud = () => {
   const dispatch = useDispatch();
   const searchCrud = useSelector((state) => state.crud.search);
+  const inputRef = useRef("");
 
-  const handleSearchCrud = async (e) => {
-    e.preventDefault();
-    let searched =
-      e.target.firstElementChild.nextElementSibling.value.toUpperCase();
+  const handleSearchCrud = async () => {
+    let searched = inputRef.current.value.toUpperCase();
     let movieSearched = [];
     await db
       .collection("movies")
@@ -25,16 +24,20 @@ const SearchCrud = () => {
             ...hijo.data(),
           });
         });
-      })
+      });
     dispatch(crudSearch(movieSearched));
   };
 
   return (
     <Box py="20px" maxWidth="70%" mx="auto">
-      <HStack as="form" onSubmit={handleSearchCrud}>
+      <HStack>
         <GoHomeIcon />
-        <Input type="text" placeholder="Nombre de la película..." />
-        <Button bgColor="brand.primary" type="submit">
+        <Input
+          ref={inputRef}
+          type="text"
+          placeholder="Nombre de la película..."
+        />
+        <Button bgColor="brand.primary" onClick={handleSearchCrud}>
           Buscar
         </Button>
       </HStack>
