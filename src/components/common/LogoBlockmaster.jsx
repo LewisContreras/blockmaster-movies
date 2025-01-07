b3e3b0dd3fd0134dd09239488c64bc56fd7916d5
Lewis Contreras <legiconba4@gmail.com>
2025-01-06 22:11:18 -0500

    refac(all)#3: rm unused code
    - Remove unused jsons
    - Fix export

--- src/components/common/LogoBlockmaster.jsx
+++ src/components/common/LogoBlockmaster.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Img } from "@chakra-ui/image";
 
-export const LogoBlockmaster = () => {
+const LogoBlockmaster = () => {
     return (
         <Img src="https://i.imgur.com/pwIYVhf.png" alt="Logo de Blockmaster" />
     );
